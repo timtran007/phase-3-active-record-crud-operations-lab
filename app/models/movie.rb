d9abeb889cf72ac5c5346e6e4b45c23d51dd4f26
Tim <timtran007@gmail.com>
2022-12-24 00:34:59 -0600

    commit tree 701b198a202bb79394dd04582bb4e0f9c52d2a50

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,46 @@
+require 'pry'
 class Movie < ActiveRecord::Base
+    def self.create_with_title(title)
+        self.create(title: title)
+    end
 
+    def self.first_movie
+        self.first
+    end
+
+    def self.last_movie
+        self.last
+    end
+
+    def self.movie_count
+        self.count
+    end
+
+    def self.find_movie_with_id(id)
+        self.find(id)
+    end
+
+    def self.find_movie_with_attributes(attribute)
+        self.find_by(attribute)
+    end
+
+    def self.find_movies_after_2002
+        self.where("release_date > 2002")
+    end
+
+    def update_with_attributes (attribute)
+        self.update(attribute)
+    end
+
+    def self.update_all_titles (t)
+        self.update(title: t)
+    end
+
+    def self.delete_by_id (id)
+        self.find(id).destroy
+    end
+
+    def self.delete_all_movies
+        self.destroy_all
+    end
 end
